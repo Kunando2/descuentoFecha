65ba0a36f7a84efa4d7eae9a9fa26cef3ddacc0b
GolomaxBufano <fer_bostero_91@hotmail.com>
2024-11-27 16:20:14 -0300

    agragado de diferencia de 45 dias

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ function calcularDescuento(fechaVencimiento) {
     const diferenciaMilisegundos = new Date(fechaVencimiento) - hoy;
     const diferenciaDias = Math.floor(diferenciaMilisegundos / (1000 * 60 * 60 * 24));
 
-    if (diferenciaDias > 45) 
-        { return 'sin_descuento'; } 
-    else if (diferenciaDias >= 30) { return 0.30; } 
-    else if (diferenciaDias >= 15) { return 0.40; } 
-    else if (diferenciaDias >= 7) { return 0.50; } 
-    else if (diferenciaDias >= 0) { return 0.70; } 
-    else { return 0.70; }
+    if (diferenciaDias > 45) {
+        return 'sin_descuento';
+    } else if (diferenciaDias >= 30) {
+        return 0.30;
+    } else if (diferenciaDias >= 15) {
+        return 0.40;
+    } else if (diferenciaDias >= 7) {
+        return 0.50;
+    } else if (diferenciaDias >= 0) {
+        return 0.70;
+    } else {
+        return 0.70;
+    }
 }
 
 function calcularPrecioFinal(precio, descuento) {
@@ -29,9 +35,13 @@ function calcular(event) {
     }
 
     const descuento = calcularDescuento(fechaVencimientoInput);
-    const precioFinal = calcularPrecioFinal(precioInput, descuento);
-
-    document.getElementById('resultado').innerText = `El descuento es del ${descuento * 100}%. El precio final con descuento es: $${precioFinal.toFixed(2)}`;
+    
+    if (descuento === 'sin_descuento') {
+        document.getElementById('resultado').innerText = `No posee descuento. El precio es: $${precioInput.toFixed(2)}`;
+    } else {
+        const precioFinal = calcularPrecioFinal(precioInput, descuento);
+        document.getElementById('resultado').innerText = `El descuento es del ${descuento * 100}%. El precio final con descuento es: $${precioFinal.toFixed(2)}`;
+    }
 }
 
 // Añadir el evento de escucha al formulario
